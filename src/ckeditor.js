7a53eb60af51eb53761b20fe1098e93049be9f20
Mickaël Tordjman <mickael.tdj@gmail.com>
2021-05-15 18:49:10 +0200

    fix math engine

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -133,6 +133,15 @@ ClassicEditor.defaultConfig = {
 		],
 		shouldNotGroupWhenFull: true
 	},
+	math: {
+		engine: 'mathjax', // or katex or function. E.g. (equation, element, display) => { ... }
+		lazyLoad: undefined, // async () => { ... }, called once before rendering first equation if engine doesn't exist. After resolving promise, plugin renders equations.
+		outputType: 'script', // or span
+		forceOutputType: false, // forces output to use outputType
+		enablePreview: true, // Enable preview view
+		previewClassName: [], // Class names to add to previews
+		popupClassName: [] // Class names to add to math popup balloon
+	},
 	image: {
 		styles: [
 			'alignLeft',
